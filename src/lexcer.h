5410ba80cc8fee81daf8ca273bef94a6490d217d
Your Name <you@example.com>
2024-05-29 12:03:21 +0200

    Dummy commit:  - Efficient lexer

--- src/lexcer.h
+++ src/lexcer.h
@@ -3,85 +3,20 @@
 
 #include <iostream>
 #include <map>
+#include <optional>
 #include <string>
 #include <vector>
 
 enum class TokenType {
-  _return,
-  EOF_,
-  _if,
-  _else,
-  _while,
-  _for,
-  _break,
-  _continue,
+  _exit,
   _int,
-  _float,
-  _char,
-  _string,
-  _bool,
-  _void,
-  _true,
-  _false,
-  _null,
-  _new,
-  _delete,
-  _this,
-  _super,
-  _class,
-  _interface,
-  _extends,
-  _implements,
-  _public,
-  _protected,
-  _private,
-  _static,
-  _final,
-  _abstract,
-  _native,
-  _synchronized,
   _semicolon,
-  _comma,
-  _dot,
 };
 
-const std::map<TokenType, std::string> TokenTypeString = {
-    {TokenType::_return, "return"},
-    {TokenType::EOF_, "EOF_"},
-    {TokenType::_if, "if"},
-    {TokenType::_else, "else"},
-    {TokenType::_while, "while"},
-    {TokenType::_for, "for"},
-    {TokenType::_break, "break"},
-    {TokenType::_continue, "continue"},
-    {TokenType::_int, "int"},
-    {TokenType::_float, "float"},
-    {TokenType::_char, "char"},
-    {TokenType::_string, "string"},
-    {TokenType::_bool, "bool"},
-    {TokenType::_void, "void"},
-    {TokenType::_true, "true"},
-    {TokenType::_false, "false"},
-    {TokenType::_null, "null"},
-    {TokenType::_new, "new"},
-    {TokenType::_delete, "delete"},
-    {TokenType::_this, "this"},
-    {TokenType::_super, "super"},
-    {TokenType::_class, "class"},
-    {TokenType::_interface, "interface"},
-    {TokenType::_extends, "extends"},
-    {TokenType::_implements, "implements"},
-    {TokenType::_public, "public"},
-    {TokenType::_protected, "protected"},
-    {TokenType::_private, "private"},
-    {TokenType::_static, "static"},
-    {TokenType::_final, "final"},
-    {TokenType::_abstract, "abstract"},
-    {TokenType::_native, "native"},
-    {TokenType::_synchronized, "synchronized"},
+const std::map<TokenType, std::string> TokenTypeMap = {
+    {TokenType::_exit, "exit"},
+    {TokenType::_int, "exit"},
     {TokenType::_semicolon, "semicolon"},
-    {TokenType::_comma, "comma"},
-    {TokenType::_dot, "dot"},
 };
 
 struct Token {
@@ -89,138 +24,67 @@ struct Token {
   std::string value;
 
   std::tuple<std::string, std::string> getToken() {
-    return std::make_tuple(TokenTypeString.at(type), value);
+    return std::make_tuple(TokenTypeMap.at(type), value);
+  }
+};
+class Lexer {
+private:
+  const std::string m_src{};
+  int m_curr{};
+
+  std::optional<char> peak(int ahead = 1) const {
+    if (m_curr + ahead >= m_src.size()) {
+      return {};
+    } else {
+      return m_src.at(m_curr);
+    }
+  };
+
+  char consume() {
+    char curr_char = m_src.at(m_curr);
+    m_curr += 1;
+    return curr_char;
+  }
+  void restart_to_start_of_file() { m_curr = 0; }
+
+public:
+  explicit Lexer(std::string &src) : m_src(src) {}
+
+  std::vector<Token> lexical_analysis() {
+    std::vector<Token> tokens;
+    std::string buff;
+    while (peak().has_value()) {
+      if (std::isalpha(peak().value())) {
+        buff.push_back(consume());
+        while (peak().has_value() && std::isalnum(peak().value())) {
+          buff.push_back(consume());
+        }
+        if (buff == "exit") {
+          tokens.push_back(Token{TokenType::_exit, buff});
+        } else {
+          throw std::runtime_error("Error: Token not found!");
+        }
+        buff.clear();
+      } else if (isspace(peak().value())) {
+        consume();
+        buff.clear();
+      } else if (isdigit(peak().value())) {
+        buff.push_back(consume());
+        while (peak().has_value() && isdigit(peak().value())) {
+          buff.push_back(consume());
+        }
+        tokens.push_back(Token{TokenType::_int, buff});
+        buff.clear();
+      } else if (peak().value() == ';') {
+        consume();
+        tokens.push_back(Token{TokenType::_semicolon});
+      } else {
+        throw std::runtime_error("Error: Token not found!");
+      }
+    }
+    restart_to_start_of_file();
+    return tokens;
   }
 };
 
-std::vector<Token> lexical_analysis(std::string &str) {
-  std::vector<Token> tokens;
-  std::string buff;
-  for (int i = 0; i < str.size(); i++) {
-
-
-    while (isalpha(str[i])) {
-      buff.push_back(str[i]);
-      i++;
-    }
-    if (buff == "return") {
-      tokens.push_back(Token{TokenType::_return, buff});
-      buff.clear();
-    } else if (buff == "EOF") {
-      tokens.push_back(Token{TokenType::EOF_, buff});
-      buff.clear();
-    } else if (buff == "if") {
-      tokens.push_back(Token{TokenType::_if, buff});
-      buff.clear();
-    } else if (buff == "else") {
-      tokens.push_back(Token{TokenType::_else, buff});
-      buff.clear();
-    } else if (buff == "while") {
-      tokens.push_back(Token{TokenType::_while, buff});
-      buff.clear();
-    } else if (buff == "for") {
-      tokens.push_back(Token{TokenType::_for, buff});
-      buff.clear();
-    } else if (buff == "break") {
-      tokens.push_back(Token{TokenType::_break, buff});
-      buff.clear();
-    } else if (buff == "continue") {
-      tokens.push_back(Token{TokenType::_continue});
-      buff.clear();
-    } else if (buff == "int") {
-      tokens.push_back(Token{TokenType::_int, buff});
-      buff.clear();
-    } else if (buff == "float") {
-      tokens.push_back(Token{TokenType::_float, buff});
-      buff.clear();
-    } else if (buff == "char") {
-      tokens.push_back(Token{TokenType::_char, buff});
-      buff.clear();
-    } else if (buff == "string") {
-      tokens.push_back(Token{TokenType::_string, buff});
-      buff.clear();
-    } else if (buff == "bool") {
-      tokens.push_back(Token{TokenType::_bool, buff});
-      buff.clear();
-    } else if (buff == "void") {
-      tokens.push_back(Token{TokenType::_void, buff});
-      buff.clear();
-    } else if (buff == "true") {
-      tokens.push_back(Token{TokenType::_true, buff});
-      buff.clear();
-    } else if (buff == "false") {
-      tokens.push_back(Token{TokenType::_false, buff});
-      buff.clear();
-    } else if (buff == "null") {
-      tokens.push_back(Token{TokenType::_null, buff});
-      buff.clear();
-    } else if (buff == "new") {
-      tokens.push_back(Token{TokenType::_new, buff});
-      buff.clear();
-    } else if (buff == "delete") {
-      tokens.push_back(Token{TokenType::_delete, buff});
-      buff.clear();
-    } else if (buff == "this") {
-      tokens.push_back(Token{TokenType::_this, buff});
-      buff.clear();
-    } else if (buff == "super") {
-      tokens.push_back(Token{TokenType::_super});
-      buff.clear();
-    } else if (buff == "class") {
-      tokens.push_back(Token{TokenType::_class, buff});
-      buff.clear();
-    } else if (buff == "extends") {
-      tokens.push_back(Token{TokenType::_extends, buff});
-      buff.clear();
-    } else if (buff == "implements") {
-      tokens.push_back(Token{TokenType::_implements, buff});
-      buff.clear();
-    } else if (buff == "public") {
-      tokens.push_back(Token{TokenType::_public, buff});
-      buff.clear();
-    } else if (buff == "protected") {
-      tokens.push_back(Token{TokenType::_protected, buff});
-      buff.clear();
-    } else if (buff == "private") {
-      tokens.push_back(Token{TokenType::_private, buff});
-      buff.clear();
-    } else if (buff == "static") {
-      tokens.push_back(Token{TokenType::_static, buff});
-      buff.clear();
-    } else if (buff == "final") {
-      tokens.push_back(Token{TokenType::_final, buff});
-      buff.clear();
-    } else if (buff == "abstract") {
-      tokens.push_back(Token{TokenType::_abstract, buff});
-      buff.clear();
-    } else if (buff == "native") {
-      tokens.push_back(Token{TokenType::_native, buff});
-      buff.clear();
-    } else if (buff == "synchronized") {
-      tokens.push_back(Token{TokenType::_synchronized, buff});
-      buff.clear();
-    }
-
-    if (isspace(str[i])) {
-      continue;
-    }
-
-    while (isdigit(str[i])) {
-      buff.push_back(str[i]);
-      i++;
-    }
-
-    if (buff.size() > 0 && std::all_of(buff.begin(), buff.end(), ::isdigit)) {
-      tokens.push_back(Token{TokenType::_int, buff});
-      buff.clear();
-    }
-
-    if (str[i] == ';') {
-      tokens.push_back(Token{TokenType::_semicolon});
-      buff.clear();
-      i++;
-    }
-  }
-  return tokens;
-}
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,13 @@ std::string tokens_to_x86_asm(std::vector<Token> &tokens) {
   std::stringstream out;
   out << "global _start\n_start:\n";
   for (int i = 0; i < tokens.size(); i++) { // return <int>;
-    if (tokens.at(i).type == TokenType::_return) {
+    if (tokens.at(i).type == TokenType::_exit) {
       if (i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::_int) {
         if (i + 2 < tokens.size() &&
             tokens.at(i + 2).type == TokenType::_semicolon) {
           // https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
           out << "    mov rax, 60\n"; // exit code
-          out << "    mov rdi, " << tokens.at(i + 1).value
-          << "\n";
+          out << "    mov rdi, " << tokens.at(i + 1).value << "\n";
           out << "    syscall\n";
         }
       }
@@ -42,17 +41,29 @@ int main(int argc, char **argv) {
     contents << file.rdbuf();
     str = contents.str();
   }
-  std::vector<Token> tokens{};
-  tokens = lexical_analysis(str);
+  Lexer lex(str);
+
+  std::vector<Token> tokens = lex.lexical_analysis();
+
   for (auto token : tokens) {
+    std::cout << "Tokens:\n";
     std::cout << std::get<0>(token.getToken()) << " : "
               << std::get<1>(token.getToken()) << std::endl;
-    // token.toPrint();
   }
+  std::cout << "-----------------\n\n";
+
   std::string assembly = tokens_to_x86_asm(tokens);
   std::cout << assembly;
-  std::ofstream file("output.asm");
+  std::ofstream file("src/output.asm");
   file << assembly;
   file.close();
+
+  // nasm -f elf64 src/output.asm -o src/output.o && ld src/output.o -o
+  // src/output
+  system("echo 'nasm -f elf64 src/output.asm -o src/output.o && ld "
+         "src/output.o -o src/output' ");
+  system("nasm -f elf64 src/output.asm -o src/output.o && ld src/output.o -o "
+         "src/output");
+
   return EXIT_SUCCESS;
 }
